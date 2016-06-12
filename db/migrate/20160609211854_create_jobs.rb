081ce14424150b17d5c71766b61fb4d4e6ff4c55
Kevin Story <kevinjstory@live.com>
2016-06-12 13:42:19 +0000

    Added 4 columns to db migrate file

--- db/migrate/20160609211854_create_jobs.rb
+++ db/migrate/20160609211854_create_jobs.rb
@@ -3,6 +3,10 @@ class CreateJobs < ActiveRecord::Migration
     create_table :jobs do |t|
       t.string 'jobNumber'
       t.text 'jobDescription'
+      t.strin 'contactNumber'
+      t.string 'contactName'
+      t.string 'contact email'
+      t.string 'companyName'
     end
   end
   
