1260d5ab2a2189b444b30c9c138d48d56e7b3dc0
Kevin Story <kevinjstory@live.com>
2016-07-23 23:10:02 +0000

    Added user creation model/controller/table/etc

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  
+  
+  validates_uniqueness_of :email
+  
+  validates :password,
+    :length => { :minimum => 3, :if => :validate_password? },
+    :confirmation => { :if => :validate_password? }
+
+  private
+
+  def validate_password?
+    password.present? || password_confirmation.present?
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160609211854) do
+ActiveRecord::Schema.define(version: 20160723203131) do
 
   create_table "jobs", force: :cascade do |t|
     t.date   "dateCreated"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20160609211854) do
     t.string "contactName"
     t.string "contactNumber"
     t.string "contactEmail"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
