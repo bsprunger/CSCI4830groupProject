653e6a5df1f6b7a654d04f7292808423d7198149
Kevin Story <kevinjstory@live.com>
2016-07-27 02:07:38 +0000

    Added controls to edit functions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,15 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: "Logged In!"
+      redirect_to root_url, notice: "Logged In!", :style => "float: right;"
     else
-      flash.now.alert = "Email or password is invalid"
+      flash[:notice] = "Invalid username/password combination"
       render "new"
     end
   end
   
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: "Logged Out!"
+    redirect_to root_url, notice: "Logged Out!", :style => "float: right;"
   end
 end
