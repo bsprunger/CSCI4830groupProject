6e6276ef2b6589ebcf189581b99a260779adf254
Kevin Story <kevinjstory@live.com>
2016-08-04 01:22:08 +0000

    Added comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,17 +2,22 @@ class SessionsController < ApplicationController
   def new
   end
   
+  #Authentication for user submitted in login information
   def create
     user = User.find_by_email(params[:email])
+    
+    # If user email and password match what is in the user table, store the user ID in a session and display a logged in message
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged In!", :style => "float: right;"
+    #If wrong password give an error
     else
       flash[:notice] = "Invalid username/password combination"
       render "new"
     end
   end
   
+  #Allow log out, change the session id to nil
   def destroy
     session[:user_id] = nil
     redirect_to root_url, notice: "Logged Out!", :style => "float: right;"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ActiveRecord::Base
+  # Adds authentication support using password_digest column in the user table
   has_secure_password
   
-  
+  # Ensure only one account per email address
   validates_uniqueness_of :email
   
+  # Validation for passowrd, email, etc.  password confirmation should be handled by has_secure_password
   validates :password,
     :length => { :minimum => 3, :if => :validate_password? },
     :confirmation => { :if => :validate_password? }
 
+  
+  #Working on password confirmation
   private
 
   def validate_password?
